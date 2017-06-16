0899eb84d37775ffcbed80d74f6bdce3631d0d11
Taras <taraslevitskyi@gmail.com>
2017-06-16 18:21:32 +0300

    init project added page structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,73 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+//pages
+import { AboutPage } from '../pages/about/about';
+import { HomePage } from '../pages/home/home';
+import { ContactPage } from '../pages/contact/contact';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  rootPage:any = TabsPage;
+  pages: Array<{title: string, component: any, index: number}>;
+
+  constructor(
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen,
+    public menu: MenuController
+  ) {
+    this.initializeApp();
+
+    this.pages = [
+      { title: 'Home', component: HomePage, index: 0 },
+      { title: 'About', component: AboutPage, index: 1 },
+      { title: 'Contact', component: ContactPage, index: 2 },
+    ];
+  }
+
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
+  }
+
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+
+    let params = {};
+    // the nav component was found using @ViewChild(Nav)
+    // setRoot on the nav to remove previous pages and only have this page
+    // we wouldn't want the back button to show in this scenario
+    if (page.index) {
+      params = { tabIndex: page.index };
+    }
+
+    // If we are already on tabs just change the selected tab
+    // don't setRoot again, this maintains the history stack of the
+    // tabs even if changing them from the menu
+    if (this.nav.getActiveChildNav() && page.index != undefined) {
+      this.nav.getActiveChildNav().select(page.index);
+      // Set the root of the nav with params if it's a tab index
+    } else {
+      this.nav.setRoot('TabsPage', params).catch((err: any) => {
+        console.log(`Didn't set nav root: ${err}`);
+      });
+    }
+
   }
 }
