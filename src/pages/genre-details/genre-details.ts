4e6d3ef13db9cfba75c1810d6a55a3ffe44ad5e4
Taras <taraslevitskyi@gmail.com>
2017-06-29 19:08:13 +0300

    added styles for correcting list

--- src/pages/genre-details/genre-details.ts
+++ src/pages/genre-details/genre-details.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Http, URLSearchParams } from '@angular/http';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as SC from 'soundcloud';
 
 /**
  * Generated class for the GenreDetailsPage page.
@@ -17,6 +18,8 @@ export class GenreDetailsPage {
   genre: any;
   clientId: string = '2t9loNQH90kzJcsFCODdigxfp325aq4z';
   musicList: any;
+  topRate: string = 'top';
+  kindOfTrendingMusic: string = this.topRate;
 
   constructor(
     public navCtrl: NavController,
@@ -25,11 +28,17 @@ export class GenreDetailsPage {
   }
 
   ionViewDidLoad() {
+    SC.initialize({
+      client_id: this.clientId
+    });
     this.genre = this.navParams.get('data');
+    this.getPlaylist();
+  }
 
+  getPlaylist(){
     let response_url = 'https://api-v2.soundcloud.com/charts';
     let params: URLSearchParams = new URLSearchParams();
-    params.set('kind', 'top');
+    params.set('kind', this.kindOfTrendingMusic);
     params.set('genre', this.genre.data);
     params.set('limit', '50');
     params.set('linked_partitioning', '1');
@@ -37,13 +46,31 @@ export class GenreDetailsPage {
 
     this.http.get(response_url, {search:params})
       .map((res) => res.json()).subscribe(response => {
-        console.log(response);
-        this.musicList = response.collection;
+      console.log(response);
+      this.musicList = response.collection;
     });
   }
 
   goBack() {
     this.navCtrl.pop();
+  }
+
+  selectRateChart(rate) {
+    console.log(rate);
+    this.kindOfTrendingMusic = rate.value;
+    this.getPlaylist();
+  }
+
+  playTrack(item) {
+    console.log(item);
+
+    SC.stream('/tracks/'+ item.id).then(function(player){
+      console.log(player);
+      player.play();
+      player.on('time', (time) => {
+        console.log(player.currentTime());
+      });
+    });
   }
 
 }
